a68e79433505391ec9d92a2a3543131a41398bb3
Ajay <ajayvishwakarma457@gmail.com>
2025-02-24 21:08:32 +0530

    changes

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+// Dummy data
+let items = [
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+];
+
+// Get all items
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+// Get single item by ID
+app.get("/items/:id", (req, res) => {
+  const item = items.find((i) => i.id === parseInt(req.params.id));
+  if (!item) return res.status(404).json({ message: "Item not found" });
+  res.json(item);
+});
+
+// Create new item
+app.post("/items", (req, res) => {
+  const newItem = { id: items.length + 1, name: req.body.name };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+// Update item
+app.put("/items/:id", (req, res) => {
+  const item = items.find((i) => i.id === parseInt(req.params.id));
+  if (!item) return res.status(404).json({ message: "Item not found" });
+  item.name = req.body.name;
+  res.json(item);
+});
+
+// Delete item
+app.delete("/items/:id", (req, res) => {
+  items = items.filter((i) => i.id !== parseInt(req.params.id));
+  res.json({ message: "Item deleted" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
