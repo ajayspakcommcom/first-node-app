ad745d07b9e83196551f6bd5735510c093fc56a7
Ajay <ajayvishwakarma457@gmail.com>
2025-02-25 20:30:50 +0530

    changes

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ let items = [
   { id: 3, name: "Item 100" },
   { id: 4, name: "Item 2000" },
   { id: 5, name: "Item 3000" },
-  { id: 6, name: "Item 5000" },
-  { id: 7, name: "Item 9000" }
+  { id: 6, name: "Item 5000" }
 ];
 
 // Get all items
