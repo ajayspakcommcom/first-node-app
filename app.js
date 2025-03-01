5cd2cd7c01731e737b785a71ea5f69011ed23e5d
Ajay Spak <45287044+ajayspakcommcom@users.noreply.github.com>
2025-03-01 12:35:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(express.json());
 // Dummy data
 let items = [
   { id: 1, name: "Item 1" },
-  { id: 2, name: "Item 2" }
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
 ];
 
 // Get all items
