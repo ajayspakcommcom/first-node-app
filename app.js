795289747462450726ba7d9aed80586a9482cc49
Ajay Spak <45287044+ajayspakcommcom@users.noreply.github.com>
2025-04-18 21:32:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,10 +51,12 @@ app.delete("/items/:id", (req, res) => {
   res.json({ message: "Item deleted" });
 });
 
-const port = 4000;
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+//const port = 4000;
+//app.listen(port, '0.0.0.0', () => {
+//    console.log(`Server running on http://0.0.0.0:${port}`);
+//});
 // http://13.203.97.44:8080/github-webhook/
 
